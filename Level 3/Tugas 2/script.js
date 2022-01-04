e46d2bf42261216d143418be79586625acfd54da
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-04 18:49:22 +0700

    Added Level 3 Tugas 2" "

---
+++ Level 3/Tugas 2/script.js
@@ -0,0 +1,13 @@
+/*
+Algoritma adalah sebuah kumpulan instruksi yang bertujuan
+untuk menyelesaikan masalah
+*/
+
+console.log("Algoritma memasak mie instant")
+console.log("1.Siapkan mie instant,air sebanyak 500ml, piring dan tirisan ")
+console.log("2.Masak Air sebanyak 500ml hingga mendidih")
+console.log("3.Selagi Menunggu Air Mendidih, buka bungkus mie instant, kemudian tuangkan seluruh bumbunya ke dalam piring")
+console.log("4.Masukkan mie dalam air yang sudah mendidih")
+console.log("5. Setelah 3 Menit Tiriskan mie hingga kering, kemudian tuangkan mie diatas piring yang telah diberi bumbu")
+console.log("6. Aduk mie hingga bumbunya merata")
+console.log("7. Mie Instant siap disantap")
