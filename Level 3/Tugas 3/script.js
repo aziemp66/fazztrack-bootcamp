6dd4ffc927e7d28cf75693328a4d3af2238a88f0
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-04 19:00:04 +0700

    Added Level 3 Tugas 3

---
+++ Level 3/Tugas 3/script.js
@@ -0,0 +1,21 @@
+/*
+Cara membuat function di javascript :
+
+function namaFunction(parameterFunction <= (opsional) ){
+  isi dari function
+  return <= (opsional)
+}
+membuat function di javascript dilaksanakan dengan keyword function, kemudian diikuti dengan nama function kemudian buka kurung yang berisi parameter function, fucntion bisa tidak memiliki parameter atau memiliki satu atau lebih parameter
+
+function bisa juga mengembalikan nilai dengan menggunakan keyword "return"
+
+kemudian function harus dipanggil kembali untuk di eksekusi, dengan syntax sebagai berikut :
+namaFunction(parameterFunction)
+*/
+
+function salam(nama){
+  return console.log("Halo " + nama + ", Selamat pagi")
+}
+
+salam("Azie");
+
